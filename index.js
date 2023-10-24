81cde45ea6201296d16a9f1ed1b2dca9681e9c71
Rashed967 <rashedhasan967@gmail.com>
2023-10-24 18:11:45 +0600

    all are done

--- index.js
+++ index.js
@@ -44,11 +44,39 @@ async function run() {
         res.send(result)
     })
 
+    // get singl user 
+    app.get('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    })
+
     // crate user 
     app.post('/users', async(req, res) => {
         const user = req.body;
         const result = await userCollection.insertOne(user)
         res.send(result)
+    })
+
+
+    // updat user 
+    app.put('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const {name, email, gender, status} = req.body;
+      const filter = {_id : new ObjectId(id)}
+      const options = {upsert : true}
+      const updateDoc = {
+        $set : {
+          name, 
+          email, 
+          gender, 
+          status
+        }
+      }
+
+      const result = await userCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
     })
 
 
